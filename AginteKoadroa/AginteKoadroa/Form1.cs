f075a56fa579bb66a7f862ceb8a022992f66c44d
Alain Basterra <basterra.alain@uni.eus>
2022-10-10 08:41:44 +0200

    UwU

---
+++ AginteKoadroa/AginteKoadroa/Form1.cs
@@ -0,0 +1,42 @@
+﻿using AginteKoadroa.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AginteKoadroa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (var db = new SalmentaDbContext())
+            {
+                var bezeroaData = db.Bezeroa //Bezeroa ipini behar da, ez izena.
+                .GroupBy(b => b.Izena)
+                .ToDictionary(g => g.Key, g => g.Count()); // SELECT Izena, COUNT(Izena)  FROM Bezeroa  GROUP BY Izena
+                if (bezeroaData != null)
+                {
+                    if (bezeroaData.Count > 0)
+                    {
+                        chart1.DataSource = bezeroaData;
+                        chart1.Series[0].YValueMembers = "Value";
+                        chart1.Series[0].XValueMember = "Key";
+                        chart1.DataBind();
+                    }
+                }
+            }
+
+        }
+    }
+}
